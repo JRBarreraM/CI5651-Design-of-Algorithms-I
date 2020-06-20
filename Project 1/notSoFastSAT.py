22a6810b5f45445114959d6cdd92b08b0dae1e56
JRBarreraM <mrjrbarreram@gmail.com>
2020-06-19 22:16:48 -0400

    Read ejemplito listo

--- Project 1/notSoFastSAT.py
+++ Project 1/notSoFastSAT.py
@@ -1,19 +1,32 @@
+#import numpy as np
+
 file = open("ejemplitoInputSAT.txt", "r")
-print(file.readlines())
-
 lines = file.readlines()
-
 
 numOfClauses = 0
 numOfVariables = 0
-
+variables = []
+clausulas = []
+actualClausula = []
 for line in lines:
     if line[0] == 'c':
         pass
     elif line[0] == 'p':
-       line[:-2].split()
-       numOfClauses = line[2]
-       numOfVariables = line[3]
-
+        line =  line.split()
+        numOfClauses = int(line[2])
+        numOfVariables = int(line[3])
+        variables = [0] * numOfVariables
     else:
-        line.split()
+        line =  line.split()
+        for elem in line:
+            if elem != "0":
+                actualClausula.append(elem)
+            else:
+                clausulas.append(actualClausula)
+                actualClausula = []
+
+if len(actualClausula) != 0:
+    clausulas.append(actualClausula)
+
+print(clausulas)
+print(variables)
